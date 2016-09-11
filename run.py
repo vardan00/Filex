45fc557773c3333ff4b731c2d01c029877995e41
vardan00 <lella.vardan@gmail.com>
2016-09-11 09:13:43 +0530

    done with uploading

--- run.py
+++ run.py
@@ -1,10 +1,20 @@
 import os
-from flask import Flask
+from flask import *
+from werkzeug import secure_filename
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello world'
-
+@app.route('/', methods=['GET', 'POST'])
+def start():
+	return render_template("index.html")
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+	file = request.files['file']
+	if request.method == 'POST':	
+		to_disk_file_name = os.path.join(app.config['UPLOAD_DIR'], file.filename)
+		print(to_disk_file_name)
+		file.save(to_disk_file_name)
+	return ""
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 5555, debug = True)
+	app.config['UPLOAD_DIR'] = os.getcwd()
+	app.run(host = '0.0.0.0', port = 5555, debug = True)
